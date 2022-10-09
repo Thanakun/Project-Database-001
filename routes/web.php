788f22f74a36ddca71b7dc415779c5b0712c714c
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-09 15:53:31 +0700

    adjust var name

--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,8 @@ Route::group(['namespace' => 'App\Http\Controllers'], function() {
         Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
 
         Route::get('/cart', 'Bookcontroller@cart')->name('cart');
-        Route::get('/add-to-cart/{id}', 'Bookcontroller@addToCart')->name('add.to.cart');
-        Route::delete('/remove-from-cart/{id}', 'Bookcontroller@remove')->name('remove.from.cart');
+        Route::get('/add-to-cart/{ISBN}', 'Bookcontroller@addToCart')->name('add.to.cart');
+        Route::delete('/remove-from-cart/{ISBN}', 'Bookcontroller@remove')->name('remove.from.cart');
     });
 
 
