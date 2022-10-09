32dc9af0d1d4b52000492947f7cfa1397cf9ea08
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-09 16:38:06 +0700

    add isbn and flag

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -18,6 +18,7 @@ class BookSeeder extends Seeder
 
         $books = [
             [
+                'ISBN' => '9786163371140',
                 'name' => 'Re:Zero - Restarting Life from Zero in Another World Volume 1',
                 'cover' => 'https://cdn-local.mebmarket.com/meb/server1/157947/Thumbnail/book_detail_large.gif?2',
                 'price' => 171,
@@ -26,9 +27,12 @@ class BookSeeder extends Seeder
                 'publisher_date' => '2015-10-1 00:00:00',
                 'description' => 'description text',
                 'author_name' => 'Nagatsuki Tappei',
-                'stock' => 100
+                'stock' => 100,
+                'manga_flag' => false,
+                'novel_flag' => true
             ],
             [
+                'ISBN' => '9786163371539',
                 'name' => 'Re:Zero - Restarting Life from Zero in Another World Volume 2',
                 'cover' => 'https://cdn-local.mebmarket.com/meb/server1/157948/Thumbnail/book_detail_large.gif?2',
                 'price' => 198,
@@ -37,7 +41,23 @@ class BookSeeder extends Seeder
                 'publisher_date' => '2015-10-1 00:00:00',
                 'description' => 'description text',
                 'author_name' => 'Nagatsuki Tappei',
-                'stock' => 100
+                'stock' => 100,
+                'manga_flag' => false,
+                'novel_flag' => true
+            ],
+            [
+                'ISBN' => '9786164474093',
+                'name' => 'Fly Me to The Moon Volume 1',
+                'cover' => 'https://cdn1.mangaqube.com/O47BADSHBM9JDA58_l',
+                'price' => 80,
+                'pages_num' => 198,
+                'publisher_name' => 'LuckPim',
+                'publisher_date' => '2019-03-30 00:00:00',
+                'description' => 'description text',
+                'author_name' => 'Kenjiro Hata',
+                'stock' => 100,
+                'manga_flag' => true,
+                'novel_flag' => false
             ]
             
         ];
