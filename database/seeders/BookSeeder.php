aeb94f5ba8eb7a0675a5051eabfcf5168e741514
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-09 15:52:55 +0700

    make seeder create author and book

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -1,8 +1,12 @@
 <?php
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
-class ProductSeeder extends Seeder
+use App\Models\Book;
+use App\Models\Author;
+
+use Illuminate\Support\Facades\DB;
+
+class BookSeeder extends Seeder
 {
     /**
     * Run the database seeds.
@@ -11,32 +15,44 @@ class ProductSeeder extends Seeder
     */
     public function run()
     {
+
         $books = [
             [
                 'name' => 'Re:Zero - Restarting Life from Zero in Another World Volume 1',
+                'cover' => 'https://cdn-local.mebmarket.com/meb/server1/157947/Thumbnail/book_detail_large.gif?2',
+                'price' => 171,
+                'pages_num' => 449,
+                'publisher_name' => 'A Plus',
+                'publisher_date' => '2015-10-1 00:00:00',
                 'description' => 'description text',
-                'image' => 'https://cdn-local.mebmarket.com/meb/server1/157947/Thumbnail/book_detail_large.gif?2',
+                'author_name' => 'Nagatsuki Tappei',
+                'stock' => 100
+            ],
+            [
+                'name' => 'Re:Zero - Restarting Life from Zero in Another World Volume 2',
+                'cover' => 'https://cdn-local.mebmarket.com/meb/server1/157948/Thumbnail/book_detail_large.gif?2',
                 'price' => 198,
                 'pages_num' => 449,
                 'publisher_name' => 'A Plus',
-                'publisher_date' => '10-01-2015',
-                'publisher_name' => 198,
-                'publisher_name' => 198,
-                'publisher_name' => 198,
-                'stock' => 999
-            ],
-            [
-                'name' => 'Apple iPhone 12',
-                'description' => 'Apple Brand',
-                'image' => 'https://dummyimage.com/200x300/000/fff&text=Iphone',
-                'stock' => 999,
-                'price' => 500
+                'publisher_date' => '2015-10-1 00:00:00',
+                'description' => 'description text',
+                'author_name' => 'Nagatsuki Tappei',
+                'stock' => 100
             ]
             
         ];
         
+
         foreach ($books as $key => $value) {
-            Book::create($value);
+
+            if(!DB::table('authors')->where('name',$value['author_name'])->exists())
+                Author::create(['name' => $value['author_name']]);
+
+            if(!DB::table('books')->where('name',$value['name'])->exists()){
+                $value['author_id'] = DB::table('authors')->where('name',$value['author_name'])->pluck('id')[0];
+                Book::create($value);
+            }
+            
         }
     }
 }
